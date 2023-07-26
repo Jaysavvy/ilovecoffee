ed5d4a71ac695c91d89759328e676f92947337b3
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-07-26 16:01:05 -0400

    added some new functions to the API

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { ApiKeyGuard } from './common/gaurds/api-key/api-key.guard';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
@@ -14,7 +18,18 @@ async function bootstrap() {
     }
   }),
   );
+  app.useGlobalInterceptors(new WrapResponseInterceptor)
+  // app.useGlobalGuards(new ApiKeyGuard())
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application is our Rest API. Access all of our data through here and create fun projects')
+    .setVersion('1.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
